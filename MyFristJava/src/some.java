6a711f905d56d09fc3fbec86cc1b35ac7244c645
rakesh <rakesh.hadoop12@gmail.com>
2016-08-02 07:39:47 -0700

    This is my Intial program

---
+++ MyFristJava/src/some.java
@@ -0,0 +1,15 @@
+
+public class some {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+		}
+		private static void displayGreeting() {
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This is rakesh");
+	}
+
+}
